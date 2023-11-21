79ad044c527d8fbb919c5ac09534973b859189a2
AewInformatica <aewinformatica@gmail.com>
2023-11-21 16:43:33 -0300

    add exemples

--- assemblyx64/assemblyx64/CombineA.asm
+++ assemblyx64/assemblyx64/CombineA.asm
@@ -1,20 +1,10 @@
 .data
-realVal REAL8 +1.5
+realVal REAL8 1.0
 
 .code
 PUBLIC CombineA
 CombineA proc
 
-add ecx,dword ptr [rsp+28h]
-add ecx,r9d
-add ecx,r8d
-add ecx,edx
-
-;Convert Packed Doubleword Integers to Packed Double Precision Floating-PointValues
-cvtdq2pd xmm0, xmm0
-movsd xmm1,realVal
-addsd xmm1,mmword ptr [rsp+30h]
-divsd xmm0,xmm1
 ret
 CombineA endp
 end
--- assemblyx64/assemblyx64/main.cpp
+++ assemblyx64/assemblyx64/main.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 
 using namespace std;
-extern "C" int CaixaD();
-extern "C" double CombineA(int a,int b,int c,int d,int e,double f);
 extern "C" int testeASM();
-
-int i;
 
 int main(){
  
-cout << "CombineA:" << CombineA(1,2,3,4,5,6.1) <<endl;
-for(i=0;i<11;i++){
+for(int i=0;i<11;i++){
 cout << " " << testeASM()<<endl;
 }
-CaixaD();
+system("PAUSE");
 return 0;
 }
